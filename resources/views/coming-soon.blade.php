61631e73097ddf6efdaeab591d90fb42df7c0491
Rik van Noord <rik@heerlijk.nl>
2015-07-20 11:42:45 +0200

    Title Change
    Changed title of coming-soon page.

--- resources/views/coming-soon.blade.php
+++ resources/views/coming-soon.blade.php
@@ -1,6 +1,6 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en"><head>
 
-    <title>We'Rotterdam</title>
+    <title>We'Rotterdam Coming soon!</title>
 
     <meta http-equiv="content-type" content="text/html;charset=utf-8">
     <meta http-equiv="cache-control" content="no-cache">
